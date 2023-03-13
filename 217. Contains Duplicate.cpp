57fc212bfa95e0b3d27db739dae3d6969e573b21
Dragon Of Flame <dipusultan.ds@gmail.com>
2023-03-14 04:34:25 +0600

    3 way

--- 217. Contains Duplicate.cpp
+++ 217. Contains Duplicate.cpp
@@ -19,6 +19,51 @@ public:
     }
 };
 
+/*class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        vector<int>v;
+        v=nums;
+        sort(v.begin(), v.end());
+        bool f=false;
+        for(int i=0; i<v.size()-1; i++)
+        {
+            if(v[i]==v[i+1]) f=true;
+        }
+        if(f==true) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+};*/
+
+/*class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        vector<int>v;
+        v=nums;
+        sort(v.begin(), v.end());
+        bool f=false;
+        for(int i=0; i<v.size()-1; i++)
+        {
+            if(v[i]==v[i+1]) f=true;
+        }
+        if(f==true) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+};*/
 int main() {
     Solution solution;
     std::vector<int> nums = {1,1,1,3,3,4,3,2,4,2};
