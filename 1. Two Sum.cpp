f91cac698945e4b86a736efb2fe63372828eca31
Dragon Of Flame <dipusultan.ds@gmail.com>
2023-03-17 03:11:20 +0600

    Easy

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -17,7 +17,42 @@ public:
         return {};
     }
 };
+/*class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<pair<int,int>>x;
 
+        for(int i=0;i<(int)nums.size();i++) 
+        {
+            x.push_back({nums[i],i});
+        }
+        
+        sort(x.begin(),x.end());
+
+        vector<int>ans;
+        int i=0,j=(int)nums.size()-1;
+
+        while(i<j)
+        {
+            int a=x[i].first+x[j].first;
+            if(a==target)
+            {
+                ans.push_back(x[i].second);
+                ans.push_back(x[j].second);
+
+                return ans;
+            }
+            if(a>target)
+            {
+                j--;
+            }
+            else i++;
+        }
+
+        return ans;
+    }
+};*/
 int main()
 {
     Solution sol;
