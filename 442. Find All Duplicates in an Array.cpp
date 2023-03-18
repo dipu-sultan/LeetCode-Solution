214112fe5391f913172a890e651acaf398c62bb4
Dragon Of Flame <dipusultan.ds@gmail.com>
2023-03-19 01:07:48 +0600

    Array Easy

--- 442. Find All Duplicates in an Array.cpp
+++ 442. Find All Duplicates in an Array.cpp
@@ -1,6 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<pair<int, int>>mp;
+        unordered_map<int, int>m;
+        vector<int>v;
+        for(int x: nums)
+        {
+            m[x]++;
+        }
+
+        for(auto i=m.begin(); i!=m.end(); i++)
+        {
+            if(i->second==2)
+            {
+                mp.push_back({i->first, i->second});
+            }
+        }
+        sort(mp.begin(), mp.end());
+
+        for(int i=0; i<mp.size(); i++)
+        {
+            v.push_back(mp[i].first);
+        }
+
+        return v;
+
+    }    
+};
+
+/*
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        map<int, int>mp;
+        priority_queue<int>pq, v;
+      
+        for(auto x: nums)
+        {
+            pq.push(x);
+        }
+        int fe=pq.top();
+        pq.pop()
+        while(!pq)
+        {
+            int se=pq.top();
+            if(fe==se)
+            {
+                v.push(se);
+                pq.pop()
+            }
+            else
+            {
+                fi=se;
+                pq.pop();
+            }
+        }
+        return v;
+    }
+};
+
+*/
+
+/*
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
@@ -22,10 +87,16 @@ public:
     }
 };
 
+*/
 int main()
 {
     Solution sol;
 
-    vector<int>v={4,3,2,7,8,2,3,1};
-    sol.findDuplicates(v);
+    vector<int>x,v={4,3,2,7,8,2,3,1};
+    x=sol.findDuplicates(v);
+    for(int i=0; i<x.size(); i++)
+    {
+        cout << x[i] << " ";
+    }
+    cout << endl;
 }
